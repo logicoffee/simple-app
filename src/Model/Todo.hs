cdcab18205ce4c0ca0fac9762a0663a865b5ae16
Ryo Iketani <38097603+logicoffee@users.noreply.github.com>
2019-05-11 13:53:47 +0900

    Crud statements (#6)
    * Add packages
    
    * Define CRUD statements

--- src/Model/Todo.hs
+++ src/Model/Todo.hs
@@ -6,15 +6,21 @@
 
 module Model.Todo where
 
-import           Data.Aeson.Types                (FromJSON, ToJSON)
-import           Data.Text.Lazy                  (Text)
-import           Data.Time.Calendar              (Day)
+import           Data.Aeson.Types                     (FromJSON, ToJSON)
+import           Data.Text.Lazy                       (Text)
+import           Data.Time.Calendar                   (Day)
 
-import           Database.HDBC.Query.TH          (defineTableFromDB',
-                                                  makeRelationalRecord)
-import           Database.HDBC.Schema.PostgreSQL (driverPostgreSQL)
-import           GHC.Generics                    (Generic)
-import           Model.DB                        (connectPG)
+import           Database.HDBC.Query.TH               (defineTableFromDB',
+                                                       makeRelationalRecord)
+import           Database.HDBC.Schema.PostgreSQL      (driverPostgreSQL)
+import           GHC.Generics                         (Generic)
+import           Model.DB                             (connectPG)
+
+import           Data.Functor.ProductIsomorphic.Class ((|$|), (|*|))
+import           Database.Relational.Monad.Class      (wheres)
+import           Database.Relational.Pi               (Pi)
+import           Database.Relational.Projectable      (placeholder, (!), (.=.))
+import qualified Database.Relational.Type             as RType
 
 defineTableFromDB'
     connectPG
@@ -32,5 +38,29 @@ data Todo' = Todo'
     , pDeadline :: !Day
     } deriving (Generic)
 
+makeRelationalRecord ''Todo'
+
+piTodo' :: Pi Todo Todo'
+piTodo' = Todo'
+    |$| task'
+    |*| deadline'
+
 instance ToJSON Todo
 instance FromJSON Todo'
+
+findAll :: RType.Query () Todo
+findAll = RType.relationalQuery todo
+
+findById :: RType.Query Int Todo
+findById = selectTodo
+
+create :: RType.Insert Todo'
+create = RType.insert piTodo'
+
+update :: RType.KeyUpdate Int Todo
+update = updateTodo
+
+delete :: RType.Delete Int
+delete = RType.delete $ \rec -> do
+    (ph, _) <- placeholder $ \phRec -> wheres $ rec ! id' .=. phRec
+    return ph

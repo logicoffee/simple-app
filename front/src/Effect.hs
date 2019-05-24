272be5b279bef93d37f289892de9c716659aec32
Ryo Iketani <38097603+logicoffee@users.noreply.github.com>
2019-05-24 21:37:38 +0900

    Define basic effects (#16)
    * Define basic effects
    
    * Fix

---
+++ front/src/Effect.hs
@@ -0,0 +1,28 @@
+module Effect where
+
+import           API
+import           Common.Types.Todo
+import           Miso.Effect
+
+data Model = Model
+    { todos       :: [Todo] -- Todo全体
+    , draftCreate :: Todo'  -- 新規作成中のTodo
+    }
+
+data Action = FetchTodos
+            | DisplayAll [Todo]
+            | UpdateDraftCreate Todo'
+            | CreateTodo
+            | FinishTodo TodoID
+
+updateModel :: Action -> Model -> Effect Action Model
+updateModel FetchTodos model              = model <# fmap DisplayAll fetchAllTodos
+updateModel (DisplayAll tds) model        = noEff $ model { todos = tds }
+updateModel (UpdateDraftCreate td') model = noEff $ model { draftCreate = td' }
+updateModel CreateTodo model = model <# do
+    td <- createTodo $ draftCreate model
+    let tds = todos model
+    return $ DisplayAll $ td : tds
+updateModel (FinishTodo tid) model = model <# do
+    deleteTodo tid
+    return FetchTodos
